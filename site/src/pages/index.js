3d39d697c4b85ec8ab686131948c3c37e05ee1f9
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-09-01 18:25:34 -0300

    up sites

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Widget_topBar from "@/components/topBar/widget";
 import Widget_Lista_animais from "@/components/Lista_animais/widget";
 import Widget_Banner from "@/components/banner/widget";
