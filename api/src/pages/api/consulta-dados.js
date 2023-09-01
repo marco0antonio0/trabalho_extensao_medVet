91eb86d043cd73c9a1bf5ec21c2c25cbe3ca4317
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-09-01 17:49:31 -0300

    up files api

--- api/src/pages/api/consulta-dados.js
+++ api/src/pages/api/consulta-dados.js
@@ -1,5 +1,17 @@
 import DatabaseConnection from "./../../model/models";
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   const db = new DatabaseConnection();
   try {
     await db.connect();
